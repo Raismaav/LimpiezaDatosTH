95adf637802242982779e06f350ddc487ad333c8
Rafael Magaña <raismaav.2510@gmail.com>
2024-04-03 14:26:10 -0600

    Organized files

--- analisisTraficoSISA.py
+++ analisisTraficoSISA.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Lectura del dataset de entrenamiento
-df = pd.read_csv('prb.csv', header=None)
+df = pd.read_csv('SISA Trafic/prb.csv', header=None)
 
 # Obtenemos datos de la primera fila y lo guardamos em headers
 
